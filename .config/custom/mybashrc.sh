a084323a5c061a8b8efb69f576f9a89ba535ff55
Shameera <shameerayodage@gmail.com>
2024-03-31 16:05:07 -0700

    Add custom bashrc configs

---
+++ .config/custom/mybashrc.sh
@@ -0,0 +1,21 @@
+######## NVIM Configs ################
+export PATH="$PATH:/opt/nvim-linux64/bin"
+. "$HOME/.cargo/env"
+
+if type nvim > /dev/null 2>&1; then
+    alias vim=nvim
+fi
+
+export EDITOR=nvim
+export VISUAL=nvim
+
+######## Alacrity Configs ################
+source ~/.bash_completion/alacritty
+
+
+######## Aliases ################
+#
+alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ full|percentage"'
+# Dotfile Configs
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
