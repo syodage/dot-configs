6f845c8148b072845d2c27e62e08aa163c078274
Shameera <shameerayodage@gmail.com>
2024-03-31 17:42:47 -0700

    Add starship prompt

--- .config/custom/mybashrc.sh
+++ .config/custom/mybashrc.sh
@@ -1,9 +1,11 @@
+############ CUSTOM CONFIGS ######################
+
 ######## NVIM Configs ################
 export PATH="$PATH:/opt/nvim-linux64/bin"
 . "$HOME/.cargo/env"
 
-if type nvim > /dev/null 2>&1; then
-    alias vim=nvim
+if type nvim >/dev/null 2>&1; then
+	alias vim=nvim
 fi
 
 export EDITOR=nvim
@@ -12,10 +14,11 @@ export VISUAL=nvim
 ######## Alacrity Configs ################
 source ~/.bash_completion/alacritty
 
-
 ######## Aliases ################
-#
 alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ full|percentage"'
 # Dotfile Configs
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
+######## Startship Configs ######
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
+eval "$(starship init bash)"
